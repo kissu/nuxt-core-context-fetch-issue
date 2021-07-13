5505d0f70161e63eeb33f020f2fde79586f456d0
Konstantin BIFERT <konstantin.b@worklife.io>
2021-07-13 13:00:05 +0200

    fix some typos

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    <div v-if="!$fetchState.pending">fetched data!</div>
+    <template v-if="isFetchingStillPending">fetched data!</template>
     <div v-else>fetching still....</div>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData({ isDev }) {
-    console.log('is dev ?', isDev)
+  async fetch({ isDev }) {
+    console.log('is dev', isDev)
     const call = await fetch('https://jsonplaceholder.typicode.com/todos/1')
     const res = await call.json()
     console.log('res', res)
-  }
+  },
+  computed: {
+    isFetchingStillPending() {
+      return !this.$fetchState?.pending
+    }
+  },
 }
 </script>
