5caabfb6657dc8134b7a849efde6ff5025c946ee
Konstantin BIFERT <konstantin.b@worklife.io>
2021-07-13 12:08:30 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-if="!$fetchState.pending">fetched data!</div>
+    <div v-else>fetching still....</div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ isDev }) {
+    console.log('is dev ?', isDev)
+    const call = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const res = await call.json()
+    console.log('res', res)
+  }
+}
+</script>
